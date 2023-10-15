437fff7660e8d1420ddf8263f3ed8567637ec58f
Sachini Rasanga <sachisach983@gmail.com>
2023-10-15 20:49:04 +0530

    update_train

--- src/components/User_management/NaveBar.js
+++ src/components/User_management/NaveBar.js
@@ -12,7 +12,7 @@ export default function NaveBar() {
             <button><a href="/train" style={{ textDecoration: "none" }}>Train Management</a></button>
             <button><a href="/" style={{ textDecoration: "none" }}>Shedule Management</a></button>
             <button><a href="/travelerprofile" style={{ textDecoration: "none" }}>Traveler Account Management</a></button>
-            <button><a href="/" style={{ textDecoration: "none" }}>Booking Management</a></button>
+            <button><a href="/booking" style={{ textDecoration: "none" }}>Booking Management</a></button>
            
         </div>
 
